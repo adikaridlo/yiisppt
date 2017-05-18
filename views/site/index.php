368ad05a4ac6ad86afb77268638b0f0b273b1863
adikaridlo <arsuga02@gmail.com>
2017-05-18 11:10:40 +0700

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -8,6 +8,7 @@ $this->title = 'My Yii Application';
 
     <div class="jumbotron">
         <h1>Congratulations!</h1>
+        <h1> Halo Mas Brooo</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
@@ -17,7 +18,7 @@ $this->title = 'My Yii Application';
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-lg-6">
                 <h2>Heading</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
